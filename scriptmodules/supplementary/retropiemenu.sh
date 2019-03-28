fea92f005f436afab0ca532e595c72742e4c5168
microplay <48363872+mpcore2@users.noreply.github.com>
2019-03-28 07:24:46 +0100

    helpers - add getIPAddress() function

--- scriptmodules/supplementary/retropiemenu.sh
+++ scriptmodules/supplementary/retropiemenu.sh
@@ -156,8 +156,8 @@ function launch_retropiemenu() {
             printMsgs "dialog" "Your runcommand.log is:\n\n$(cat /dev/shm/runcommand.log)"
             ;;
 	showip.rp)
-            local ip="$(ip route get 8.8.8.8 2>/dev/null | head -1 | cut -d' ' -f8)"
-            printMsgs "dialog" "Your IP is: $ip\n\nOutput of 'ip addr show':\n\n$(ip addr show)"
+            local ip="$(getIPAddress)"
+            printMsgs "dialog" "Your IP is: ${ip:-(unknown)}\n\nOutput of 'ip addr show':\n\n$(ip addr show)"
             ;;
         *.rp)
             rp_callModule $no_ext depends
