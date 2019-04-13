758f642871d9d5fc45b9959629d3497c2f3ba185
microplay <48363872+mpcore-nxt@users.noreply.github.com>
2019-04-13 18:35:59 +0200

    Update reicast.sh

--- scriptmodules/emulators/reicast/reicast.sh
+++ scriptmodules/emulators/reicast/reicast.sh
@@ -72,11 +72,6 @@ function mapInput() {
     echo "$params"
 }
 
-if [[ ! -f "$HOME/RetroPie/BIOS/dc/dc_boot.bin" ]]; then
-    dialog --no-cancel --pause "You need to copy the Dreamcast BIOS files (dc_boot.bin and dc_flash.bin) to the folder $biosdir to boot the Dreamcast emulator." 22 76 15
-    exit 1
-fi
-
 params=(-config config:homedir=$home -config x11:fullscreen=1)
 getAutoConf reicast_input && params+=($(mapInput))
 [[ -n "$AUDIO" ]] && params+=(-config audio:backend=$AUDIO -config audio:disable=0)
