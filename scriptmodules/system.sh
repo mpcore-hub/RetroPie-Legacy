81f8d8d284be69e949656b7f1839e26799e8e6a6
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-09 07:52:10 +0200

    add sun50i

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The Microplay Project 
+# based on The RetroPie Project
+#
+# only for sun8i, sun50i (like Allwinner H2+/H3/A64/H5)
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -268,29 +271,20 @@ function get_platform() {
                     __platform="rpi2"
                 fi
                 ;;
-            ODROIDC)
-                __platform="odroid-c1"
-                ;;
-            ODROID-C2)
-                __platform="odroid-c2"
-                ;;
-             sun8i)
-                __platform="H3-mali"
-                ;;
-            "Freescale i.MX6 Quad/DualLite (Device Tree)")
-                __platform="imx6"
-                ;;
-            ODROID-XU3)
-                __platform="odroid-xu"
-                ;;
-            "Rockchip (Device Tree)")
-                __platform="tinker"
-                ;;
             Vero4K)
                 __platform="vero4k"
                 ;;
             "Allwinner sun8i Family")
                 __platform="sun8i"
+                ;;
+             sun8i)
+                __platform="sun8i"
+                ;;
+            "Allwinner sun50i Family")
+                __platform="sun50i"
+                ;;
+             sun50i)
+                __platform="sun50i"
                 ;;
             *)
                 case $architecture in
@@ -343,49 +337,6 @@ function platform_rpi3-64() {
     __platform_flags="arm armv8 neon rpi gles"
 }
 
-function platform_odroid-c1() {
-    __default_cflags="-O2 -mcpu=cortex-a5 -mfpu=neon-vfpv4 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
-    __default_asflags=""
-    __default_makeflags="-j2"
-    __platform_flags="arm armv7 neon mali gles"
-    __qemu_cpu=cortex-a9
-    __has_binaries=0
-}
-
-function platform_odroid-c2() {
-    __default_cflags="-O2 -march=native -ftree-vectorize -funsafe-math-optimizations"
-    __default_asflags=""
-    __default_makeflags="-j4"
-    __platform_flags="aarch64 armv8 mali"
-    __qemu_cpu=cortex-a15
-    __has_binaries=0
-    if [[ "$(getconf LONG_BIT)" -eq 32 ]]; then
-        __default_cflags="-O2 -march=armv8-a+crc -mtune=cortex-a53 -mfpu=neon-fp-armv8"
-        __platform_flags="arm armv8 neon mali gles"
-    else
-        __default_cflags="-O2 -march=native"
-        __platform_flags="aarch64 mali gles"
-    fi
-}
-
-function platform_odroid-xu() {
-    __default_cflags="-O2 -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
-    # required for mali-fbdev headers to define GL functions
-    __default_cflags+=" -DGL_GLEXT_PROTOTYPES"
-    __default_asflags=""
-    __default_makeflags="-j2"
-    __platform_flags="arm armv7 neon mali gles"
-}
-
-function platform_tinker() {
-    __default_cflags="-O2 -marm -march=armv7-a -mtune=cortex-a17 -mfpu=neon-vfpv4 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
-    # required for mali headers to define GL functions
-    __default_cflags+=" -DGL_GLEXT_PROTOTYPES"
-    __default_asflags=""
-    __default_makeflags="-j2"
-    __platform_flags="arm armv7 neon kms gles"
-}
-
 function platform_x86() {
     __default_cflags="-O2 -march=native"
     __default_asflags=""
@@ -426,12 +377,12 @@ function platform_sun8i() {
     __platform_flags="arm armv7 neon mali sun8i gles"
 }
 
-function platform_imx6() {
-    __default_cflags="-O2 -march=armv7-a -mfpu=neon -mtune=cortex-a9 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
+function platform_sun50i() {
+    __default_cflags="-O2 -march=armv8-a+crc -mtune=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations"
     __default_asflags=""
-    __default_makeflags="-j2"
-    __platform_flags="arm armv7 neon"
+    __default_makeflags="-j$(nproc)"
     __has_binaries=0
+    __platform_flags="arm armv8 neon mali sun50i gles"
 }
 
 function platform_vero4k() {
