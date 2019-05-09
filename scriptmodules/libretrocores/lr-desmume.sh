9b75f0fe145f1f1567b63f728b84d27b2abc1b39
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-09 08:49:18 +0200

    lr-desmume.sh add sun8i v1

--- scriptmodules/libretrocores/lr-desmume.sh
+++ scriptmodules/libretrocores/lr-desmume.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The Microplay Project 
+# based on The RetroPie Project
+#
+# only for sun8i, sun50i (like Allwinner H2+/H3/A64/H5)
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -22,7 +25,7 @@ function sources_lr-desmume() {
 function build_lr-desmume() {
     cd desmume/src/frontend/libretro
     local params=()
-    isPlatform "arm" && params+=("platform=armvhardfloat")
+    isPlatform "sun8i" && params+=("platform=classic_armv7_a7")
     make clean
     make "${params[@]}"
     md_ret_require="$md_build/desmume/src/frontend/libretro/desmume_libretro.so"
