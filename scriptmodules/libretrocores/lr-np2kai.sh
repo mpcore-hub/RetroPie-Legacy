e88bbff5845d00fedfbf01a7760086820090a048
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-09 09:21:30 +0200

    lr-np2kai.sh add sun8i v1

--- scriptmodules/libretrocores/lr-np2kai.sh
+++ scriptmodules/libretrocores/lr-np2kai.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The Microplay Project 
+# based on The RetroPie Project
+#
+# only for sun8i, sun50i (like Allwinner H2+/H3/A64/H5)
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -20,7 +23,7 @@ function sources_lr-np2kai() {
 
 function build_lr-np2kai() {
     cd ./sdl2
-    make
+    make platform=classic_armv7_a7 ARCH=arm
     md_ret_require="$md_build/sdl2/np2kai_libretro.so"
 }
 
