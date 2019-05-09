42128b2eb2cc534972700546be0271fc18d0f92c
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-09 09:16:45 +0200

    lr-mgba.sh add sun8i v1

--- scriptmodules/libretrocores/lr-mgba.sh
+++ scriptmodules/libretrocores/lr-mgba.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The Microplay Project 
+# based on The RetroPie Project
+#
+# only for sun8i, sun50i (like Allwinner H2+/H3/A64/H5)
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -22,10 +25,10 @@ function sources_lr-mgba() {
 
 function build_lr-mgba() {
     make -f Makefile.libretro clean
-    if isPlatform "neon"; then
-        make -f Makefile.libretro HAVE_NEON=1
+    if isPlatform "sun8i"; then
+        make -f Makefile.libretro platform=classic_armv7_a7 ARCH=arm
     else
-        make -f Makefile.libretro
+        make -f Makefile.libretro HAVE_NEON=1
     fi
     md_ret_require="$md_build/mgba_libretro.so"
 }
