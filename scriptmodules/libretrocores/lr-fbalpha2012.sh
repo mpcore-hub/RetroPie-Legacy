735caa69f15c6bfaf4fa86e9e55822247b23729e
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-09 08:56:31 +0200

    lr-fbalpha2012.sh add sun8i v1

--- scriptmodules/libretrocores/lr-fbalpha2012.sh
+++ scriptmodules/libretrocores/lr-fbalpha2012.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The Microplay Project 
+# based on The RetroPie Project
+#
+# only for sun8i, sun50i (like Allwinner H2+/H3/A64/H5)
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -28,7 +31,7 @@ function build_lr-fbalpha2012() {
     cd svn-current/trunk/
     make -f makefile.libretro clean
     local params=()
-    isPlatform "arm" && params+=("platform=armv")
+    isPlatform "sun8i" && params+=("platform=classic_armv7_a7")
     make -f makefile.libretro "${params[@]}"
     md_ret_require="$md_build/svn-current/trunk/fbalpha2012_libretro.so"
 }
