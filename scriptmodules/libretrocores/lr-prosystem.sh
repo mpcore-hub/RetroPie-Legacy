6a4bf13e57f5e137f22b04836f38b30f72cf4197
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-09 09:30:38 +0200

    lr-prosystem.sh add sun8i v1

--- scriptmodules/libretrocores/lr-prosystem.sh
+++ scriptmodules/libretrocores/lr-prosystem.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The Microplay Project 
+# based on The RetroPie Project
+#
+# only for sun8i, sun50i (like Allwinner H2+/H3/A64/H5)
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
 #
 # See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
-#
-#  Many, many thanks go to all people that provide the individual modules!!!
 #
 
 rp_module_id="lr-prosystem"
@@ -23,7 +24,7 @@ function sources_lr-prosystem() {
 
 function build_lr-prosystem() {
     make clean
-    CXXFLAGS="$CXXFLAGS -fsigned-char" make
+    CXXFLAGS="$CXXFLAGS -fsigned-char" make platform=classic_armv7_a7 ARCH=arm
     md_ret_require="$md_build/prosystem_libretro.so"
 }
 
