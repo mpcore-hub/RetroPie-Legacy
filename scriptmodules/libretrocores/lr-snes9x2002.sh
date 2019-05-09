66c051eff87e87d95dd50dab7ab3f41910bf6c3e
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-09 09:39:22 +0200

    lr-snes9x2002.sh add sun8i v1

--- scriptmodules/libretrocores/lr-snes9x2002.sh
+++ scriptmodules/libretrocores/lr-snes9x2002.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The Microplay Project 
+# based on The RetroPie Project
+#
+# only for sun8i, sun50i (like Allwinner H2+/H3/A64/H5)
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -27,7 +30,7 @@ function sources_lr-snes9x2002() {
 
 function build_lr-snes9x2002() {
     make clean
-    CFLAGS="$CFLAGS -Wa,-mimplicit-it=thumb" make ARM_ASM=1
+    CFLAGS="$CFLAGS -Wa,-mimplicit-it=thumb" make platform=classic_armv7_a7 ARM_ASM=1
     md_ret_require="$md_build/snes9x2002_libretro.so"
 }
 
