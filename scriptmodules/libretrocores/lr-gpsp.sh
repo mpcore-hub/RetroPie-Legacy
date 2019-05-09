2c1556484c33b33113decbb14e13c930814c00f6
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-09 09:04:09 +0200

    lr-gpsp.sh add sun8i v1

--- scriptmodules/libretrocores/lr-gpsp.sh
+++ scriptmodules/libretrocores/lr-gpsp.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The Microplay Project 
+# based on The RetroPie Project
+#
+# only for sun8i, sun50i (like Allwinner H2+/H3/A64/H5)
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -24,7 +27,7 @@ function build_lr-gpsp() {
     make clean
     rpSwap on 512
     local params=()
-    isPlatform "arm" && params+=(platform=armv)
+    isPlatform "sun8i" && params+=(platform=classic_armv7_a7)
     make "${params[@]}"
     rpSwap off
     md_ret_require="$md_build/gpsp_libretro.so"
