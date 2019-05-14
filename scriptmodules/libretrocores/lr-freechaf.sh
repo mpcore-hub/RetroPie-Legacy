39020123053ee4a10b847a74097ead843ff37b4d
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-14 09:31:43 +0200

    Update lr-freechaf.sh

--- scriptmodules/libretrocores/lr-freechaf.sh
+++ scriptmodules/libretrocores/lr-freechaf.sh
@@ -23,8 +23,16 @@ function sources_lr-freechaf() {
 }
 
 function build_lr-freechaf() {
-    make clean
-    make platform=classic_armv7_a7 ARCH=arm
+    if isPlatform "sun8i"; then
+        make clean
+        make platform=classic_armv7_a7 ARCH=arm
+    elif isPlatform "sun50i"; then
+        make clean
+        make platform=sun50i
+    else
+        make clean
+        make
+    fi
     md_ret_require="$md_build/freechaf_libretro.so"
 }
 
