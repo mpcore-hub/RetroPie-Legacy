3a90f246fcda7fa09b0b976a571a6f2cfcfb3a30
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-14 09:14:40 +0200

    Update lr-beetle-ngp.sh

--- scriptmodules/libretrocores/lr-beetle-ngp.sh
+++ scriptmodules/libretrocores/lr-beetle-ngp.sh
@@ -28,8 +28,16 @@ function sources_lr-beetle-ngp() {
 }
 
 function build_lr-beetle-ngp() {
-    make clean
-    make platform=classic_armv7_a7 ARCH=arm
+    if isPlatform "sun8i"; then
+        make clean
+        make platform=classic_armv7_a7 ARCH=arm
+    elif isPlatform "sun50i"; then
+        make clean
+        make platform=sun50i
+    else
+        make clean
+        make
+    fi
     md_ret_require="$md_build/mednafen_ngp_libretro.so"
 }
 
