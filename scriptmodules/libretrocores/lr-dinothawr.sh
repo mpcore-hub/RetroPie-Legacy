db28415189a0846bbba3ef0fff0b689bb63e46a2
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-14 09:24:27 +0200

    Update lr-dinothawr.sh

--- scriptmodules/libretrocores/lr-dinothawr.sh
+++ scriptmodules/libretrocores/lr-dinothawr.sh
@@ -23,8 +23,16 @@ function sources_lr-dinothawr() {
 }
 
 function build_lr-dinothawr() {
-    make clean
-    make platform=classic_armv7_a7 ARCH=arm
+    if isPlatform "sun8i"; then
+        make clean
+        make platform=classic_armv7_a7 ARCH=arm
+    elif isPlatform "sun50i"; then
+        make clean
+        make platform=sun50i
+    else
+        make clean
+        make
+    fi
     md_ret_require="$md_build/dinothawr_libretro.so"
 }
 
