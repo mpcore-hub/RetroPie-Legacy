e176831e048a88c6d68b8ae5c6188299e057813a
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-14 09:15:08 +0200

    Update lr-beetle-pce-fast.sh

--- scriptmodules/libretrocores/lr-beetle-pce-fast.sh
+++ scriptmodules/libretrocores/lr-beetle-pce-fast.sh
@@ -28,8 +28,16 @@ function sources_lr-beetle-pce-fast() {
 }
 
 function build_lr-beetle-pce-fast() {
-    make clean
-    make platform=classic_armv7_a7 ARCH=arm
+    if isPlatform "sun8i"; then
+        make clean
+        make platform=classic_armv7_a7 ARCH=arm
+    elif isPlatform "sun50i"; then
+        make clean
+        make platform=sun50i
+    else
+        make clean
+        make
+    fi
     md_ret_require="$md_build/mednafen_pce_fast_libretro.so"
 }
 
