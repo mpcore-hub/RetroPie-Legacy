502bd43af8c5f5e6cef9c676ccf8bebc9b231f08
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-14 09:12:14 +0200

    Update lr-81.sh

--- scriptmodules/libretrocores/lr-81.sh
+++ scriptmodules/libretrocores/lr-81.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# Only for Allwinner H2+/H3 sun8i
+# This file is part of The Microplay Project 
+# based on The RetroPie Project
+#
+# only for sun8i, sun50i (like Allwinner H2+/H3/A64/H5)
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -20,8 +23,16 @@ function sources_lr-81() {
 }
 
 function build_lr-81() {
-    make clean
-    make platform=classic_armv7_a7 ARCH=arm
+    if isPlatform "sun8i"; then
+        make clean
+        make platform=classic_armv7_a7 ARCH=arm
+    elif isPlatform "sun50i"; then
+        make clean
+        make platform=sun50i
+    else
+        make clean
+        make
+    fi
     md_ret_require="$md_build/81_libretro.so"
 }
 
