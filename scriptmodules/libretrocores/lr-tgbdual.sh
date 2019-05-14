4fa575b3e67cb45f1e28e52d23a1ca820aab857c
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-14 18:13:23 +0200

    Update lr-tgbdual.sh

--- scriptmodules/libretrocores/lr-tgbdual.sh
+++ scriptmodules/libretrocores/lr-tgbdual.sh
@@ -23,8 +23,16 @@ function sources_lr-tgbdual() {
 }
 
 function build_lr-tgbdual() {
-    make clean
-    make platform=classic_armv7_a7 ARCH=arm
+    if isPlatform "sun8i"; then
+        make clean
+        make platform=classic_armv7_a7 ARCH=arm
+    elif isPlatform "sun50i"; then
+        make clean
+        make platform=sun50i
+    else
+        make clean
+        make
+    fi
     md_ret_require="$md_build/tgbdual_libretro.so"
 }
 
