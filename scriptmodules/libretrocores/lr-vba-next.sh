0f8c31b8e563d636ef91e55cd5ae139d75836bf1
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-14 18:14:29 +0200

    Update lr-vba-next.sh

--- scriptmodules/libretrocores/lr-vba-next.sh
+++ scriptmodules/libretrocores/lr-vba-next.sh
@@ -24,10 +24,14 @@ function sources_lr-vba-next() {
 }
 
 function build_lr-vba-next() {
-    make -f Makefile.libretro clean
     if isPlatform "sun8i"; then
-        make -f Makefile.libretro platform=classic_armv7_a7
+        make -f Makefile.libretro clean
+        make -f Makefile.libretro platform=classic_armv7_a7 ARCH=arm
+    elif isPlatform "sun50i"; then
+        make -f Makefile.libretro clean
+        make -f Makefile.libretro platform=sun50i
     else
+        make -f Makefile.libretro clean
         make -f Makefile.libretro
     fi
     md_ret_require="$md_build/vba_next_libretro.so"
