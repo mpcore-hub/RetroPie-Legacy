e060ea09880a26bb01854be3cba718918c0a9d85
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-15 19:49:11 +0200

    Update microplay.sh

--- scriptmodules/supplementary/microplay.sh
+++ scriptmodules/supplementary/microplay.sh
@@ -52,12 +52,15 @@ function gui_microplay() {
 				#install update and backup es-systems config
 				mv -f "/etc/emulationstation/es_systems.cfg" "/etc/emulationstation/es_systems.bkup"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/es_systems/." "/etc/emulationstation"
+				chmod 755 "/etc/emulationstation/es_systems.cfg"
 				#install Emulationstation system logo
 				mv -f "/opt/retropie/supplementary/emulationstation/resources/splash.svg" "/opt/retropie/supplementary/emulationstation/resources/splash.svg.bkup"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/ES-Splashscreen/." "/opt/retropie/supplementary/emulationstation/resources"
+				chmod 755 "/opt/retropie/supplementary/emulationstation/resources/splash.svg"
 				#install ES Audio-Fix
 				mv -f "/opt/retropie/configs/all/emulationstation/es_settings.cfg" "/opt/retropie/configs/all/emulationstation/es_settings.cfg.bkup"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/ES-Audiofix/." "/opt/retropie/configs/all/emulationstation/"
+				chmod 755 "/opt/retropie/configs/all/emulationstation/es_settings.cfg"
 				#copy boot and bios files
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/boot/." "/boot"
 				#install motd logo file
