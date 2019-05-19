11849247df6481b38e66c5214e15e092fa024125
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-19 09:52:05 +0200

    Update microplay.sh

--- scriptmodules/supplementary/microplay.sh
+++ scriptmodules/supplementary/microplay.sh
@@ -34,52 +34,72 @@ function gui_microplay() {
         [[ -z "$choice" ]] && break
         case "$choice" in
             1)
-				#mpcore-nxt base v1.3			
+				#mpcore-nxt base v1.3	
+				echo "install & update mpcore-nxt base"
+				echo "*"
+				echo "**"
+				echo "***"
 				#remove old System for upgrade
+				echo "remove old System for upgrade"
                 rm -rf "$datadir/retropiemenu/icons"
 		        rm -rf "/home/pi/RetrOrangePi"
 		        rm -rf "/home/pi/RetroPie/retropiemenu/RetrOrangePi"
 				#install retropiemenu iconset
+				echo "install retropiemenu iconset"
                 cp -rf "$scriptdir/scriptmodules/supplementary/retropiemenu/icons_nes" "$datadir/retropiemenu/icons"
 				#install retropiemenu gamelist
+				echo "install retropiemenu gamelist"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/retropiemenu_gamelist/." "/opt/retropie/configs/all/emulationstation/gamelists/retropie"		
 				#install mpcore data
+				echo "install mpcore data"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/Microplay/." "$datadir/retropiemenu/Microplay"
 				#install tekcommand_png runcommand images
+				echo "install tekcommand_png runcommand images"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/tekcommand_png/." "/opt/retropie/"
 				#install Screensaver images
+				echo "#install Screensaver images"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/Screensaver/." "/opt/retropie/configs/all/emulationstation"
 				#install Splashscreens images and sounds
+				echo "#install Splashscreens images and sounds"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/splashscreens/." "/home/pi/RetroPie/splashscreens"
 				chmod 755 /home/pi/RetroPie/splashscreens/*
 				#install update and backup es-systems config
+				echo "install update and backup es-systems config"
 				mv -f "/etc/emulationstation/es_systems.cfg" "/etc/emulationstation/es_systems.bkup"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/es_systems/." "/etc/emulationstation"
 				chmod 755 "/etc/emulationstation/es_systems.cfg"
 				#install Emulationstation system logo
+				echo "install Emulationstation system logo"
 				mv -f "/opt/retropie/supplementary/emulationstation/resources/splash.svg" "/opt/retropie/supplementary/emulationstation/resources/splash.svg.bkup"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/ES-Splashscreen/splash.svg" "/opt/retropie/supplementary/emulationstation/resources/splash.svg"
 				chmod 755 "/opt/retropie/supplementary/emulationstation/resources/splash.svg"
 				#install ES Audio-Fix
+				echo "install ES Audio-Fix"
 				mv -f "/opt/retropie/configs/all/emulationstation/es_settings.cfg" "/opt/retropie/configs/all/emulationstation/es_settings.cfg.bkup"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/ES-Audiofix/." "/opt/retropie/configs/all/emulationstation/"
 				chmod 755 "/opt/retropie/configs/all/emulationstation/es_settings.cfg"
-				#install BGM-Script cfg in Autostart
+				#install BGM-Script in Autostart
+				echo "install BGM-Script in Autostart"
 				mv -f "/opt/retropie/configs/all/autostart.sh" "/opt/retropie/configs/all/autostart.sh.bkup"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/BGM/autostart.sh" "/opt/retropie/configs/all/autostart.sh"
 				chmod 755 "/opt/retropie/configs/all/autostart.sh"
 				#copy boot and bios files
+				echo "copy boot and bios files"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/boot/." "/boot"
 				#install motd logo file
+				echo "install motd logo file"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/motd_logo/." "/etc/update-motd.d"
 				#install system UpdateSource
+				echo "install system UpdateSource"
                 cp -rf "$scriptdir/scriptmodules/supplementary/mpcore/system_source/." "/etc/apt"
 				#change access
+				echo "change access"
                 chown -cR pi:pi "/etc/emulationstation"
                 chown -cR pi:pi "/opt/retropie"
                 chown -cR pi:pi "/home/pi/RetroPie"
                 chown -cR pi:pi "/home/pi/RetroPie-Setup"
-
+				#finish
+				echo "mpcore-nxt installation successful"
 
                 printMsgs "dialog" "mpcore-nxt base updated\n\nRestart System to apply."
                 ;;
