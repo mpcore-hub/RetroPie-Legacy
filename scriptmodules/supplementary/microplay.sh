30b00a4e34c77f64716d7a800bbc332cf31f91ff
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-05-25 08:37:22 +0200

    Update microplay.sh

--- scriptmodules/supplementary/microplay.sh
+++ scriptmodules/supplementary/microplay.sh
@@ -192,7 +192,7 @@ function gui_microplay() {
             4)
 			#Armbian-OS Setup
 		echo "Armbian-OS Setup"
-		cmd sudo armbian-config
+		armbian-config
 		;;
             5)
 			#set BGM to default
