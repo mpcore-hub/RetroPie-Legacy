21453870e69d0387447e042635d957ba6c7df935
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-08-02 19:41:18 +0200

    Update Bezelproject.sh

--- scriptmodules/supplementary/mpcore/Microplay/Bezelproject.sh
+++ scriptmodules/supplementary/mpcore/Microplay/Bezelproject.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pushd ~/home/pi/RetroPie/retropiemenu/Microplay
+pushd ~/RetroPie/retropiemenu/Microplay
 sudo ./home/pi/RetroPie-Setup/scriptmodules/supplementary/bezelproject/bezelproject.sh
