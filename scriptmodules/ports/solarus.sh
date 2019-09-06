075f7deb9c6ffead6aea1d8a8d1c1eef5581844d
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-09-06 06:57:23 +0200

    #2832 from hhromic/finetune-solarus

--- scriptmodules/ports/solarus.sh
+++ scriptmodules/ports/solarus.sh
@@ -52,6 +52,9 @@ function build_solarus() {
     cd build
     cmake "${params[@]}" ..
     make
+    md_ret_require=(
+        "$md_build/build/solarus-run"
+    )
 }
 
 function install_solarus() {
