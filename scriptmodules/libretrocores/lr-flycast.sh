a449a240243c403309581da19d5e84d229de9de3
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-09-06 07:09:24 +0200

    update lr-flycast to def

--- scriptmodules/libretrocores/lr-flycast.sh
+++ scriptmodules/libretrocores/lr-flycast.sh
@@ -18,13 +18,15 @@ rp_module_help="Previously named lr-reicast then lr-beetle-dc\n\nDreamcast ROM E
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/flycast/master/LICENSE"
 rp_module_section="exp"
 
-#function _update_hook_lr-flycast() {
-#    renameModule "lr-reicast" "lr-beetle-dc"
-#    renameModule "lr-beetle-dc" "lr-flycast"
-#}
+function _update_hook_lr-flycast() {
+    renameModule "lr-reicast" "lr-beetle-dc"
+    renameModule "lr-beetle-dc" "lr-flycast"
+}
 
 function sources_lr-flycast() {
     gitPullOrClone "$md_build" https://github.com/libretro/flycast.git
+    # don't override our C/CXXFLAGS
+sed -i "/^C.*FLAGS.*:=/d" Makefile
 }
 
 function build_lr-flycast() {
@@ -44,6 +46,7 @@ function build_lr-flycast() {
 function install_lr-flycast() {
     md_ret_files=(
         'flycast_libretro.so'
+        'LICENSE'
     )
 }
 
@@ -53,26 +56,24 @@ function configure_lr-flycast() {
     mkUserDir "$biosdir/dc"
     
     local system
-    for system in atomiswave dreamcast naomi; do
-        mkRomDir "$system"
-        ensureSystemretroconfig "$system"
-        iniConfig " = " "" "$configdir/$system/retroarch.cfg"
-        iniSet "video_shared_context" "true"
-        iniSet "video_smooth" "true"
-        addEmulator 1 "$md_id" "$system" "$md_inst/flycast_libretro.so"
-        addSystem "$system"
+        for system in atomiswave dreamcast naomi; do
+            mkRomDir "$system"
+            ensureSystemretroconfig "$system"
+            iniConfig " = " "" "$configdir/$system/retroarch.cfg"
+            iniSet "video_shared_context" "true"
+            addEmulator 1 "$md_id" "$system" "$md_inst/flycast_libretro.so </dev/null"
+            addSystem "$system"
     done
     
-    # temp fix
-    sed -i -e 's:/opt/retropie/emulators/retroarch/bin/retroarch:/opt/retropie/emulators/retroarch/bin/retroarch < /dev/null :g' "$configdir/atomiswave/emulators.cfg"
-    sed -i -e 's:/opt/retropie/emulators/retroarch/bin/retroarch:/opt/retropie/emulators/retroarch/bin/retroarch < /dev/null :g' "$configdir/dreamcast/emulators.cfg"
-    sed -i -e 's:/opt/retropie/emulators/retroarch/bin/retroarch:/opt/retropie/emulators/retroarch/bin/retroarch < /dev/null :g' "$configdir/naomi/emulators.cfg"
+#    # temp fix
+#    sed -i -e 's:/opt/retropie/emulators/retroarch/bin/retroarch:/opt/retropie/emulators/retroarch/bin/retroarch < /dev/null :g' "$configdir/atomiswave/emulators.cfg"
+#    sed -i -e 's:/opt/retropie/emulators/retroarch/bin/retroarch:/opt/retropie/emulators/retroarch/bin/retroarch < /dev/null :g' "$configdir/dreamcast/emulators.cfg"
+#    sed -i -e 's:/opt/retropie/emulators/retroarch/bin/retroarch:/opt/retropie/emulators/retroarch/bin/retroarch < /dev/null :g' "$configdir/naomi/emulators.cfg"
 
     # set core options
     setRetroArchCoreOption "${dir_name}reicast_allow_service_buttons" "enabled"
     setRetroArchCoreOption "${dir_name}reicast_alpha_sorting" "per-triangle (normal)"
     setRetroArchCoreOption "${dir_name}reicast_analog_stick_deadzone" "15%"
-    setRetroArchCoreOption "${dir_name}reicast_audio_buffer_size" "1024"
     setRetroArchCoreOption "${dir_name}reicast_boot_to_bios" "disabled"
     setRetroArchCoreOption "${dir_name}reicast_broadcast" "NTSC"
     setRetroArchCoreOption "${dir_name}reicast_cable_type" "TV (RGB)"
@@ -83,18 +84,18 @@ function configure_lr-flycast() {
     setRetroArchCoreOption "${dir_name}reicast_enable_purupuru" "enabled"
     setRetroArchCoreOption "${dir_name}reicast_enable_rtt" "disabled"
     setRetroArchCoreOption "${dir_name}reicast_enable_rttb" "disabled"
-    setRetroArchCoreOption "${dir_name}reicast_extra_depth_scale" "disabled"
     setRetroArchCoreOption "${dir_name}reicast_framerate" "fullspeed"
     setRetroArchCoreOption "${dir_name}reicast_gdrom_fast_loading" "disabled"
+    setRetroArchCoreOption "${dir_name}reicast_hle_bios" "disabled"
     setRetroArchCoreOption "${dir_name}reicast_internal_resolution" "640x480"
-    setRetroArchCoreOption "${dir_name}reicast_mipmapping" "disabled"
+    setRetroArchCoreOption "${dir_name}reicast_mipmapping" "enabled"
     setRetroArchCoreOption "${dir_name}reicast_region" "USA"
     setRetroArchCoreOption "${dir_name}reicast_render_to_texture_upscaling" "1x"
     setRetroArchCoreOption "${dir_name}reicast_screen_rotation" "horizontal"
     setRetroArchCoreOption "${dir_name}reicast_synchronous_rendering" "enabled"
     setRetroArchCoreOption "${dir_name}reicast_system" "auto"
     setRetroArchCoreOption "${dir_name}reicast_texupscale" "off"
-    setRetroArchCoreOption "${dir_name}reicast_texupscale_max_filtered_texture_size" "256"
+    setRetroArchCoreOption "${dir_name}reicast_texupscale_max_filtered_texture_size" "1024"
     setRetroArchCoreOption "${dir_name}reicast_threaded_rendering" "enabled"
     setRetroArchCoreOption "${dir_name}reicast_trigger_deadzone" "0%"
     setRetroArchCoreOption "${dir_name}reicast_vmu1_pixel_off_color" "DEFAULT_OFF 01"
