6ccdb56b234686ab4c1d844b3df3db78e98d582e
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-09-15 11:05:58 +0200

    Update fruitbox.sh

--- scriptmodules/emulators/fruitbox.sh
+++ scriptmodules/emulators/fruitbox.sh
@@ -13,7 +13,6 @@
 #
 rp_module_id="fruitbox"
 rp_module_desc="Fruitbox - A customizable MP3 Retro Jukebox. Read the Package Help for more information."
-rp_module_help="Copy your .mp3 files to '$romdir/jukebox' then launch Fruitbox from EmulationStation.\n\nTo configure a gamepad, launch 'Jukebox Config' in Settings, then 'Enable Gamepad Configuration'."
 rp_module_section="opt"
 rp_module_flags=""
 
