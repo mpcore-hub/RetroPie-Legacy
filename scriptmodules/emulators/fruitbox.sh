3c04ca08d71e1ca5d0bbf41883a5c3325c1bce75
microplay <48363872+mpcore-hub@users.noreply.github.com>
2019-09-15 11:19:29 +0200

    Update fruitbox.sh

--- scriptmodules/emulators/fruitbox.sh
+++ scriptmodules/emulators/fruitbox.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-# This file is part of The Microplay Project 
-# based on The RetroPie Project
-#
-# only for sun8i, sun50i (like Allwinner H2+/H3/A64/H5)
-#
-# The RetroPie Project is the legal property of its developers, whose names are
-# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
-#
-# See the LICENSE.md file at the top-level directory of this distribution and
-# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
-#
+
 
 rp_module_id="fruitbox"
 rp_module_desc="Fruitbox - A customizable MP3 Retro Jukebox. Read the Package Help for more information."
